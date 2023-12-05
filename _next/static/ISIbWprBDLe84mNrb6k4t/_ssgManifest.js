8f97e26ba61f9d3a66af715229d54d8138908be9
ChowKwanhwa <30934104+ChowKwanhwa@users.noreply.github.com>
2023-12-05 12:59:43 +0800

    Add files via upload

---
+++ _next/static/ISIbWprBDLe84mNrb6k4t/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fassets\u002F[chain]\u002F[contractAddress]\u002F[tokenId]","\u002Ftrending","\u002Fzero-gas-collection\u002F[chain]\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
